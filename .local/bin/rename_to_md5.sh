0df8809c49e682aeee0cf260b409a3fdd3cc53f0
benjaminblack <benjamin@benjaminblack.me>
2024-11-03 13:04:52 +0000

    [scripts] Modify `rename_to_md5.sh` a bit

--- .local/bin/rename_to_md5.sh
+++ .local/bin/rename_to_md5.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
+# Check if an argument is provided for the length of the checksum
+if [ $# -eq 1 ]; then
+    length=$1
+else
+    length=32  # Default length for MD5 checksum
+fi
+
+# Validate that the length is a positive integer
+if ! [[ "$length" =~ ^[0-9]+$ ]] || [ "$length" -le 0 ]; then
+    echo "Error: Length must be a positive integer."
+    exit 1
+fi
+
 for file in *; do
     if [ -f "$file" ]; then
         extension="${file##*.}"
         filename=$(basename "$file" ".$extension")
         md5sum=$(md5sum "$file" | cut -d ' ' -f 1)
-        new_filename="$md5sum.$extension"
+
+        # Get the specified length of the checksum
+        short_checksum=${md5sum:0:length}
+
+        new_filename="$short_checksum.$extension"
         mv "$file" "$new_filename"
     fi
 done
-
